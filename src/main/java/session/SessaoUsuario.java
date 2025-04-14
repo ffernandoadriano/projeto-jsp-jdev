d81fc8d152bc76df143533188e609a356c447c92
Fernando Adriano <lfernando.adriano@gmail.com>
2025-04-13 23:34:01 -0300

    adicionando a estrutura de sessão

---
+++ src/main/java/session/SessaoUsuario.java
@@ -0,0 +1,34 @@
+package session;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpSession;
+import model.Usuario;
+
+public class SessaoUsuario {
+
+	private SessaoUsuario() {
+	}
+
+	public static void logar(HttpServletRequest request, Usuario usuario) {
+		HttpSession session = request.getSession(); // cria uma nova session automaticamente.
+		session.setAttribute("usuarioLogado", usuario);
+	}
+
+	public static Usuario getUsuarioLogado(HttpServletRequest request) {
+		HttpSession session = request.getSession(false); // não cria uma sessão automaticamente, apenas consulta.
+		return (session != null) ? (Usuario) session.getAttribute("usuarioLogado") : null;
+	}
+
+	public static void deslogar(HttpServletRequest request) {
+		HttpSession session = request.getSession(false); // Retorna a sessão existente, ou null se não existir (⚠️ não
+															// cria)
+		if (session != null) {
+			session.invalidate(); // invalida a sessão - Remove todos os atributos armazenados na sessão.
+		}
+	}
+
+	public static boolean isLogado(HttpServletRequest request) {
+		return getUsuarioLogado(request) != null;
+	}
+
+}
