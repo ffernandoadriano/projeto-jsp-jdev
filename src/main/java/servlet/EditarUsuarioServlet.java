634ae433b4f11fc47f1ac9634e1b9447cb5b20a9
Fernando Adriano <lfernando.adriano@gmail.com>
2025-04-20 19:56:34 -0300

    Adicionando o EditarUsuarioServlet

---
+++ src/main/java/servlet/EditarUsuarioServlet.java
@@ -0,0 +1,55 @@
+package servlet;
+
+import java.io.IOException;
+
+import dao.DaoException;
+import dao.ImagemDao;
+import dao.UsuarioDao;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import model.Imagem;
+import model.Usuario;
+import session.UsuarioLogadoSession;
+
+@WebServlet("/EditarUsuarioServlet")
+public class EditarUsuarioServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	private UsuarioDao usuarioDao = new UsuarioDao();
+	private ImagemDao imagemDao = new ImagemDao();
+
+	@Override
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		doIt(request, response);
+	}
+
+	@Override
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		doIt(request, response);
+	}
+
+	private void doIt(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+		String id = request.getParameter("id");
+
+		try {
+			Usuario usuario = usuarioDao.encontrarPorId(Long.parseLong(id),
+					UsuarioLogadoSession.getUsuarioLogado(request).getId());
+
+			Imagem imagem = imagemDao.encontrarPorId(usuario.getId(), "perfil");
+			
+			request.setAttribute("PerfilFoto", imagem);
+			request.setAttribute("usuarioSalvo", usuario);
+			request.getRequestDispatcher("/principal/cadastrar_usuario.jsp").forward(request, response);
+		} catch (DaoException | NumberFormatException e) {
+			throw new ServletException(e);
+		}
+
+	}
+
+}
