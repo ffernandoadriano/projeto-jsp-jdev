568bafa545e234b1183cd1d77a4b0353c370e6ab
Fernando Adriano <lfernando.adriano@gmail.com>
2025-04-23 23:47:13 -0300

    Adicionando a classe de Endereco

---
+++ src/main/java/model/Endereco.java
@@ -0,0 +1,106 @@
+package model;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+public class Endereco implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+
+	private Long id;
+	private String cep;
+	private String rua;
+	private String numero;
+	private String bairro;
+	private String cidade;
+	private String estado;
+	private String uf;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getCep() {
+		return cep;
+	}
+
+	public void setCep(String cep) {
+		this.cep = cep;
+	}
+
+	public String getRua() {
+		return rua;
+	}
+
+	public void setRua(String rua) {
+		this.rua = rua;
+	}
+
+	public String getNumero() {
+		return numero;
+	}
+
+	public void setNumero(String numero) {
+		this.numero = numero;
+	}
+
+	public String getBairro() {
+		return bairro;
+	}
+
+	public void setBairro(String bairro) {
+		this.bairro = bairro;
+	}
+
+	public String getCidade() {
+		return cidade;
+	}
+
+	public void setCidade(String cidade) {
+		this.cidade = cidade;
+	}
+
+	public String getEstado() {
+		return estado;
+	}
+
+	public void setEstado(String estado) {
+		this.estado = estado;
+	}
+
+	public String getUf() {
+		return uf;
+	}
+
+	public void setUf(String uf) {
+		this.uf = uf;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Endereco other = (Endereco) obj;
+		return Objects.equals(id, other.id);
+	}
+
+	@Override
+	public String toString() {
+		return "Endereco [id=" + id + ", cep=" + cep + ", rua=" + rua + ", numero=" + numero + ", bairro=" + bairro
+				+ ", cidade=" + cidade + ", estado=" + estado + ", uf=" + uf + "]";
+	}
+
+}
