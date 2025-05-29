6e7a11bfcb155f99be3f7e1c1f90f6c0922b0649
Fernando Adriano <lfernando.adriano@gmail.com>
2025-05-29 20:16:14 -0300

    Adicionando o GraficoSalariosServlet

---
+++ src/main/java/servlet/GraficoSalariosServlet.java
@@ -0,0 +1,30 @@
+package servlet;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet("/GraficoSalariosServlet")
+public class GraficoSalariosServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	@Override
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		doIt(request, response);
+	}
+
+	@Override
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		doIt(request, response);
+	}
+
+	private void doIt(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		request.getRequestDispatcher("/principal/grafico_salarios.jsp").forward(request, response);
+	}
+
+}
